55a43d9923b982339a8c0659698316ef8e92f3f9
yzh549345916 <549345916@qq.com>
2019-02-15 19:03:52 +0800

    同步debug文件夹

---
+++ xzjxhyb_DBmain/bin/Debug/Style/CustomWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace xzjxhyb_DBmain.Style
+{
+    public partial class CustomWindow
+    {
+        // 拖动  
+        private void CustomWindow_MouseLeftButtonDown(object sender, MouseEventArgs e)
+        {
+            Window win = (Window)((FrameworkElement)sender).TemplatedParent;
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                win.DragMove();
+            }
+        }
+
+        // 关闭  
+        private void CustomWindowBtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win = (MainWindow)((FrameworkElement)sender).TemplatedParent;
+            win.InitialTray();
+
+        }
+
+        // 最小化  
+        private void CustomWindowBtnMinimized_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Window win = (Window)((FrameworkElement)sender).TemplatedParent;
+            win.WindowState = WindowState.Minimized;
+        }
+
+        // 最大化、还原  
+        private void CustomWindowBtnMaxNormal_Click(object sender, RoutedEventArgs e)
+        {
+            Window win = (Window)((FrameworkElement)sender).TemplatedParent;
+            if (win.WindowState == WindowState.Maximized)
+            {
+                win.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                // 不覆盖任务栏  
+                win.MaxWidth = SystemParameters.WorkArea.Width;
+                win.MaxHeight = SystemParameters.WorkArea.Height;
+                win.WindowState = WindowState.Maximized;
+            }
+        }
+
+
+
+    }
+}
